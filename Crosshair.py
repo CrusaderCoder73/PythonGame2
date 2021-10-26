5c95ca8db06e5a86d79045c004caa0f6f33a8b18
CrusaderCoder73 <92822101+CrusaderCoder73@users.noreply.github.com>
2021-10-26 12:39:40 -0600

    Created Crosshair class
    Made a class called cross hair and created its constructor to draw an image

---
+++ Crosshair.py
@@ -0,0 +1,7 @@
+import pygame, sys
+class Crosshair(pygame.sprite.Sprite):
+    def __init__(self, width, height, pos_x, pos_y, color):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
