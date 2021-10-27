ee36ffb67cb88da13bebe3f2e2ecff011f851942
CrusaderCoder73 <92822101+CrusaderCoder73@users.noreply.github.com>
2021-10-27 10:48:05 -0600

    Added Targets to The Game
    Created a new class called target.
    In GameController I made a new target group and used a for loop to put the targets at random positions on the screen.

--- GameController.py
+++ GameController.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 from Crosshair import Crosshair
+from Targets import Target
+import random
 class GameController:
     """Game Controller Constructor"""
     def __init__(self):
@@ -15,10 +17,16 @@ class GameController:
     def run(self):
         """Run method that runs the game """
 
+        #crosshair
         crosshair = Crosshair("circle_dot.png")
         crosshair_group = pygame.sprite.Group()
         crosshair_group.add(crosshair)
 
+        #targets
+        target_group = pygame.sprite.Group()
+        for target in range(20):
+            new_target = Target("new_bullet.png", random.randrange(9, self.width), random.randrange(0, self.height))
+            target_group.add(new_target)
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,7 +36,9 @@ class GameController:
             
             self.screen.fill((0, 0 ,0))
             self.screen.blit(self.background,(0,0))
+
             crosshair_group.draw(self.screen)
+            target_group.draw(self.screen)
             crosshair_group.update()
             pygame.display.flip()
             self.clock.tick(60)
